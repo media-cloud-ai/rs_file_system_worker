a82c76009ed45a1eb801bf95747541ea5513d5b9
Marc-Antoine Arnaud <arnaud.marcantoine@gmail.com>
2018-12-17 18:58:56 +0100

    update worker with amqp_worker and new message model

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,28 @@
 
+extern crate amqp_worker;
 extern crate env_logger;
-extern crate futures;
-extern crate lapin_futures as lapin;
 #[macro_use]
 extern crate log;
 extern crate serde;
 #[macro_use]
 extern crate serde_derive;
-#[macro_use]
 extern crate serde_json;
-extern crate tokio_core;
 
-mod config;
-mod emitter;
+use amqp_worker::*;
+
 mod message;
 
-use config::*;
-use futures::future::Future;
-use futures::Stream;
-use lapin::types::FieldTable;
-use std::{thread, time};
-use std::net::ToSocketAddrs;
-use tokio_core::reactor::Core;
-use tokio_core::net::TcpStream;
-use lapin::client::ConnectionOptions;
-use lapin::channel::{BasicConsumeOptions, QueueDeclareOptions};
+#[derive(Debug)]
+struct FileSystemEvent {
+}
+
+impl MessageEvent for FileSystemEvent {
+  fn process(&self, message: &str) -> Result<u64, MessageError> {
+    message::process(message)
+  }
+}
+
+static FILE_SYSTEM_EVENT: FileSystemEvent = FileSystemEvent{};
 
 fn main() {
   let env = env_logger::Env::default()
@@ -32,93 +30,5 @@ fn main() {
  
   env_logger::Builder::from_env(env).init();
 
-  loop {
-    let amqp_hostname = get_amqp_hostname();
-    let amqp_port = get_amqp_port();
-    let amqp_username = get_amqp_username();
-    let amqp_password = get_amqp_password();
-    let amqp_vhost = get_amqp_vhost();
-    let amqp_queue = get_amqp_queue();
-    let amqp_completed_queue = get_amqp_completed_queue();
-    let amqp_error_queue = get_amqp_error_queue();
-
-    info!("Connecting...");
-    debug!("AMQP HOSTNAME: {}", amqp_hostname);
-    debug!("AMQP PORT: {}", amqp_port);
-    debug!("AMQP USERNAME: {}", amqp_username);
-    debug!("AMQP VHOST: {}", amqp_vhost);
-    debug!("AMQP QUEUE: {}", amqp_queue);
-
-    // create the reactor
-    let mut core = Core::new().unwrap();
-    let handle = core.handle();
-    let address = amqp_hostname.clone() + ":" + amqp_port.as_str();
-    let addr = address.to_socket_addrs().unwrap().next().unwrap();
-    let channel_name = amqp_queue;
-
-    let state = core.run(
-      TcpStream::connect(&addr, &handle).and_then(|stream| {
-        lapin::client::Client::connect(stream, &ConnectionOptions{
-          username: amqp_username,
-          password: amqp_password,
-          vhost: amqp_vhost,
-          ..Default::default()
-        })
-      }).and_then(|(client, heartbeat_future_fn)| {
-        let heartbeat_client = client.clone();
-        handle.spawn(heartbeat_future_fn(&heartbeat_client).map_err(|_| ()));
-
-        client.create_channel()
-      }).and_then(|channel| {
-        let id = channel.id;
-        debug!("created channel with id: {}", id);
-
-        let ch = channel.clone();
-        channel.queue_declare(&channel_name, &QueueDeclareOptions::default(), &FieldTable::new()).and_then(move |_| {
-          debug!("channel {} declared queue {}", id, channel_name);
-
-          channel.basic_consume(&channel_name, "my_consumer", &BasicConsumeOptions::default(), &FieldTable::new())
-        }).and_then(|stream| {
-          stream.for_each(move |message| {
-            let data = std::str::from_utf8(&message.data).unwrap();
-            trace!("got message: {}", data);
-
-            match message::process(data) {
-              Ok(job_response) => {
-                emitter::publish(&amqp_completed_queue, serde_json::to_string(&job_response).unwrap());
-                ch.basic_ack(message.delivery_tag);
-              }
-              Err(error) => {
-                match error {
-                  message::MessageError::FormatError(msg) => {
-                    trace!("{:?}", msg);
-                    ch.basic_reject(message.delivery_tag, true);
-                  },
-                  message::MessageError::RequirementsError(msg) => {
-                    trace!("{:?}", msg);
-                    ch.basic_reject(message.delivery_tag, true);
-                  },
-                  message::MessageError::RuntimeError(job_id, msg) => {
-                    let content = json!({
-                      "status": "error",
-                      "message": msg,
-                      "job_id": job_id,
-                    });
-                    emitter::publish(&amqp_error_queue, content.to_string());
-                    let requeue = false;
-                    ch.basic_reject(message.delivery_tag, requeue);
-                  }
-                }
-              }
-            }
-            Ok(())
-          })
-        })
-      })
-    );
-
-    debug!("{:?}", state);
-    let sleep_duration = time::Duration::new(1, 0);
-    thread::sleep(sleep_duration);
-  }
+  start_worker(&FILE_SYSTEM_EVENT);
 }
--- src/message.rs
+++ src/message.rs
@@ -1,176 +1,207 @@
 
+use amqp_worker::MessageError;
 use serde_json;
 use std::fs;
 use std::error::Error;
 use std::path::Path;
 
-#[derive(Debug, Serialize, Deserialize)]
-struct Resource {
-  paths: Vec<String>
-}
-
 #[derive(Default, Debug, Serialize, Deserialize)]
-struct Requirements {
+struct Requirement {
   paths: Option<Vec<String>>
 }
 
 #[derive(Debug, Serialize, Deserialize)]
-enum Action {
-  #[serde(rename="copy")]
-  CopyFiles,
-  #[serde(rename="remove")]
-  Remove,
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-struct JobParameters {
-  id: String,
-  #[serde(rename="type")]
-  param_type: String,
-  enable: bool,
-  default: String,
-  value: String,
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-struct Parameters {
-  action: Action,
-  #[serde(default)]
-  requirements: Requirements,
-  source: Resource,
-  parameters: Vec<JobParameters>,
+#[serde(tag = "type")]
+enum Parameter {
+  #[serde(rename = "string")]
+  StringParam{id: String, default: Option<String>, value: Option<String>},
+  #[serde(rename = "paths")]
+  PathsParam{id: String, default: Option<Vec<String>>, value: Option<Vec<String>>},
+  #[serde(rename = "requirements")]
+  RequirementParam{id: String, default: Option<Requirement>, value: Option<Requirement>},
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 struct Job {
   job_id: u64,
-  parameters: Parameters
+  parameters: Vec<Parameter>
 }
 
-#[derive(Debug, Serialize)]
-pub struct JobResponse {
-  pub job_id: u64,
-  pub status: String,
-  pub files: Vec<String>
+fn get_parameter(params: &Vec<Parameter>, key: &str) -> Option<String> {
+  for param in params.iter() {
+    match param {
+      Parameter::StringParam{id, default, value} => {
+        if id == key {
+          if let Some(ref v) = value {
+            return Some(v.clone())
+          } else {
+            return default.clone()
+          }
+        }
+      },
+      _ => {}
+    }
+  }
+  None
 }
 
-pub enum MessageError {
-  FormatError(String),
-  RuntimeError(u64, String),
-  RequirementsError(String)
-}
-
-fn check_requirements(requirements: Requirements) -> Result<(), MessageError> {
-  if requirements.paths.is_some() {
-    let required_paths :Vec<String> = requirements.paths.unwrap();
-    for path in &required_paths {
-      let p = Path::new(path);
-      if !p.exists() {
-        return Err(MessageError::RequirementsError(format!("Warning: Required file does not exists: {:?}", p)));
-      }
+fn check_requirements(params: &Vec<Parameter>) -> Result<(), MessageError> {
+  for param in params.iter() {
+    match param {
+      Parameter::RequirementParam{id, value, ..} => {
+        if id == "requirements" {
+          if let Some(Requirement{paths: Some(paths)}) = value {
+            for ref path in paths.iter() {
+              let p = Path::new(path);
+              if !p.exists() {
+                return Err(MessageError::RequirementsError(format!("Warning: Required file does not exists: {:?}", p)));
+              }
+            }
+          }
+        }
+      },
+      _ => {}
     }
   }
   Ok(())
 }
 
-pub fn process(message: &str) -> Result<JobResponse, MessageError> {
-
+pub fn process(message: &str) -> Result<u64, MessageError> {
   let parsed: Result<Job, _> = serde_json::from_str(message);
 
   match parsed {
     Ok(content) => {
       debug!("reveived message: {:?}", content);
 
-      let parameters = content.parameters;
-
-      match check_requirements(parameters.requirements) {
+      match check_requirements(&content.parameters) {
         Ok(_) => {},
-        Err(msg) => { return Err(msg); }
+        Err(message) => { return Err(message); }
       }
 
-      match parameters.action {
-        Action::Remove => return remove_files(&parameters.source.paths, content.job_id),
-        Action::CopyFiles => return copy_files(&parameters.source.paths, &parameters.parameters, content.job_id),
+      match get_parameter(&content.parameters, "action").unwrap_or("Undefined".to_string()).as_str() {
+        "remove" => {
+          remove_files(&content.parameters, content.job_id)
+        },
+        "copy_files" => {
+          copy_files(&content.parameters, content.job_id)
+        },
+        action_label => {
+          Err(MessageError::ProcessingError(content.job_id, format!("Unknown action named {}", action_label)))
+        }
       }
-
     },
     Err(msg) => {
       error!("{:?}", msg);
-      return Err(MessageError::FormatError("bad input message".to_string()));
+      Err(MessageError::RuntimeError("bad input message".to_string()))
     }
   }
 }
 
-fn remove_files(files: &Vec<String>, job_id: u64) -> Result<JobResponse, MessageError> {
-  for file in files {
-    let path = Path::new(&file);
+fn remove_files(parameters: &Vec<Parameter>, job_id: u64) -> Result<u64, MessageError> {
+  let mut source_paths = None;
+  for parameter in parameters {
+    match parameter {
+      Parameter::PathsParam{id, default, value} => {
+        if id == "source_paths" {
+          if value.is_some() {
+            source_paths = value.clone();
+          } else {
+            source_paths = default.clone();
+          }
+        }
+      }
+      _ => {}
+    }
+  }
+
+  if source_paths.is_none() {
+    return Err(MessageError::ProcessingError(job_id, "Could not remove empty source files.".to_string()))
+  }
+
+  
+  for source_path in &source_paths.unwrap() {
+    let path = Path::new(&source_path);
 
     if path.is_file() {
       match fs::remove_file(path) {
         Ok(_) => debug!("Removed file: {:?}", path),
-        Err(error) => return Err(MessageError::RuntimeError(job_id, format!("Could not remove path {:?}: {}", path, error.description())))
+        Err(error) => return Err(MessageError::ProcessingError(job_id, format!("Could not remove path {:?}: {}", path, error.description())))
       }
     } else if path.is_dir() {
       match fs::remove_dir_all(path) {
         Ok(_) => debug!("Removed directory: {:?}", path),
-        Err(error) => return Err(MessageError::RuntimeError(job_id, format!("Could not remove path {:?}: {}", path, error.description())))
+        Err(error) => return Err(MessageError::ProcessingError(job_id, format!("Could not remove path {:?}: {}", path, error.description())))
       }
     } else {
-      return Err(MessageError::RuntimeError(job_id, format!("No such a file or directory: {:?}", path)));
+      return Err(MessageError::ProcessingError(job_id, format!("No such a file or directory: {:?}", path)));
     }
   }
 
-  Ok(JobResponse{
-    job_id,
-    status: "completed".to_string(),
-    files: vec![]
-  })
+  Ok(job_id)
 }
 
-fn copy_files(files: &Vec<String>, parameters: &Vec<JobParameters>, job_id: u64) -> Result<JobResponse, MessageError> {
-
+fn copy_files(parameters: &Vec<Parameter>, job_id: u64) -> Result<u64, MessageError> {
   let mut output_directory = None;
+  let mut source_paths = None;
   for parameter in parameters {
-    if parameter.id == "output_directory" {
-      output_directory = Some(parameter.value.clone());
+    match parameter {
+      Parameter::StringParam{id, default, value} => {
+        if id == "output_directory" {
+          if value.is_some() {
+            output_directory = value.clone();
+          } else {
+            output_directory = default.clone();
+          }
+        }
+      }
+      Parameter::PathsParam{id, default, value} => {
+        if id == "source_paths" {
+          if value.is_some() {
+            source_paths = value.clone();
+          } else {
+            source_paths = default.clone();
+          }
+        }
+      }
+      _ => {}
     }
   }
 
   if output_directory.is_none() {
-    return Err(MessageError::RuntimeError(job_id, "Could not copy files without output directory.".to_string()))
+    return Err(MessageError::ProcessingError(job_id, "Could not copy files without output directory.".to_string()))
+  }
+
+  if source_paths.is_none() {
+    return Err(MessageError::ProcessingError(job_id, "Could not copy files without input sources.".to_string()))
   }
 
   let mut output_files = vec![];
 
-  for file in files {
+  for source_path in &source_paths.unwrap() {
     let od = output_directory.clone().unwrap();
-    let filename = Path::new(&file).file_name().unwrap();
+    let filename = Path::new(&source_path).file_name().unwrap();
     let output_path = Path::new(&od).join(filename);
-    info!("Copy {} --> {:?}", file, output_path);
+    info!("Copy {} --> {:?}", source_path, output_path);
 
     if let Some(parent) = output_path.parent() {
       if let Err(message) = fs::create_dir_all(parent) {
-        return Err(MessageError::RuntimeError(job_id, format!("{:?}", message)));
+        return Err(MessageError::ProcessingError(job_id, format!("{:?}", message)));
       }
     }
 
-    if let Err(message) = fs::copy(file, output_path.clone()) {
-      return Err(MessageError::RuntimeError(job_id, format!("{:?}", message)));
+    if let Err(message) = fs::copy(source_path, output_path.clone()) {
+      return Err(MessageError::ProcessingError(job_id, format!("{:?}", message)));
     }
     output_files.push(output_path.to_str().unwrap().to_string());
   }
 
-  Ok(JobResponse{
-    job_id,
-    status: "completed".to_string(),
-    files: output_files
-  })
+  Ok(job_id)
 }
 
 #[cfg(test)]
 mod tests {
   use super::*;
-  use std::io::Read;
+  use std::fs::File;
   use std::io::Write;
 
   #[test]
@@ -185,12 +216,34 @@ mod tests {
     file2.write_all(b"ABCDEF1234567890").unwrap();
     assert!(path2.exists());
 
+    let msg = r#"{
+      "parameters": [
+        {
+          "id": "requirements",
+          "type": "requirements",
+          "value": {"paths": [
+            "/tmp/file_1.tmp",
+            "/tmp/file_2.tmp"
+          ]}
+        },
+        {
+          "id": "source_paths",
+          "type": "paths",
+          "value": [
+            "/tmp/file_1.tmp",
+            "/tmp/file_2.tmp"
+          ]
+        },
+        {
+          "id": "action",
+          "type": "string",
+          "value": "remove"
+        }
+      ],
+      "job_id": 123
+    }"#;
 
-    let mut message_file = File::open("tests/message_test_remove_ok.json").unwrap();
-    let mut msg = String::new();
-    message_file.read_to_string(&mut msg).unwrap();
-
-    let result = process(msg.as_str());
+    let result = process(msg);
 
     assert!(result.is_ok());
     assert!(!path1.exists(), format!("{:?} still exists", path1));
@@ -204,24 +257,79 @@ mod tests {
     file1.write_all(b"ABCDEF1234567890").unwrap();
     assert!(path1.exists());
 
-    let mut message_file = File::open("tests/message_test_remove_error.json").unwrap();
-    let mut msg = String::new();
-    message_file.read_to_string(&mut msg).unwrap();
+    let msg = r#"{
+      "parameters": [
+        {
+          "id": "requirements",
+          "type": "requirements",
+          "value": {"paths": [
+          ]}
+        },
+        {
+          "id": "source_paths",
+          "type": "paths",
+          "value": [
+            "/tmp/file_3.tmp",
+            "/tmp/file_4.tmp"
+          ]
+        },
+        {
+          "id": "action",
+          "type": "string",
+          "value": "remove"
+        }
+      ],
+      "job_id": 124
+    }"#;
 
-    let result = process(msg.as_str());
+    let result = process(msg);
 
-    assert!(match result { Err(MessageError::RuntimeError(0, _)) => true, _ => false });
+    assert_eq!(result, Err(MessageError::ProcessingError(124, "No such a file or directory: \"/tmp/file_4.tmp\"".to_string())));
     assert!(!path1.exists(), format!("{:?} still exists", path1));
   }
 
   #[test]
   fn action_test_error() {
-    let mut msg = "{\"parameters\":{\"requirements\":{},\"source\":{\"paths\":[\"/tmp/file_x.tmp\"]}},\"job_id\": 0}";
-    let mut result = process(msg);
-    assert!(match result { Err(MessageError::RuntimeError(0, _)) => true, _ => false });
+    let mut msg = r#"{
+      "parameters": [
+        {
+          "id": "requirements",
+          "type": "requirements",
+          "value": {"paths": []}
+        },
+        {
+          "id": "source_paths",
+          "type": "paths",
+          "value": ["/tmp/file_x.tmp"]
+        }
+      ],
+      "job_id": 0
+    }"#;
 
-    msg = "{\"parameters\":{\"action\": \"bad_action\",\"requirements\":{},\"source\":{\"paths\":[\"/tmp/file_x.tmp\"]}},\"job_id\": 0}";
+    let mut result = process(msg);
+    assert_eq!(result, Err(MessageError::ProcessingError(0, "Unknown action named Undefined".to_string())));
+
+    msg = r#"{
+      "parameters": [
+        {
+          "id": "action",
+          "type": "string",
+          "value": "bad_action"
+        },
+        {
+          "id": "requirements",
+          "type": "requirements",
+          "value": {"paths": []}
+        },
+        {
+          "id": "source_paths",
+          "type": "paths",
+          "value": ["/tmp/file_x.tmp"]
+        }
+      ],
+      "job_id": 0
+    }"#;
     result = process(msg);
-    assert!(match result { Err(MessageError::RuntimeError(0, _)) => true, _ => false });
+    assert_eq!(result, Err(MessageError::ProcessingError(0, "Unknown action named bad_action".to_string())));
   }
 }
